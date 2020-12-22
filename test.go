a34e114e07dd04133427c1d113204e523c3d3c20
xihuaL <lk@tapfun.me>
2020-12-22 16:34:18 +0800

    练习

---
+++ test.go
@@ -0,0 +1,11 @@
+package main
+
+/* package main包表示一个可独立执行的程序*/
+import "fmt"
+
+/* 需要使用fmt包，fmt包实现了格式化IO的函数*/
+
+func main() {
+	/* func main() 是程序开始执行的函数 */
+	fmt.Println("你好，Hello，golang，我是西华。")
+}
